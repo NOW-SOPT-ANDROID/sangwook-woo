026b8a7ea1f33e7389cea9e50975032b1ffab335
Sangwook123 <operawook@catholic.ac.kr>
2024-04-02 23:42:45 +0900

    [feat] #1 custom plugin

---
+++ build-logic/convention/src/main/java/FeaturePlugin.kt
@@ -0,0 +1,37 @@
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.api.artifacts.VersionCatalogsExtension
+import org.gradle.kotlin.dsl.dependencies
+import org.gradle.kotlin.dsl.getByType
+
+internal class FeaturePlugin : Plugin<Project> {
+    override fun apply(target: Project) {
+        with(target) {
+            with(pluginManager){
+                apply("sopt.android.library")
+                apply("sopt.android.hilt")
+            }
+
+            val libs = extensions.getByType<VersionCatalogsExtension>().named("libs")
+            dependencies {
+                "implementation"(libs.findLibrary("appcompat").get())
+                "implementation"(libs.findLibrary("core.ktx").get())
+                "implementation"(libs.findLibrary("constraintlayout").get())
+                "implementation"(libs.findLibrary("navigation.fragment.ktx").get())
+                "implementation"(libs.findLibrary("navigation.ui.ktx").get())
+
+                "implementation"(libs.findLibrary("androidx.lifecycle.runtime.ktx").get())
+                "implementation"(libs.findLibrary("androidx.lifecycle.viewmodel.ktx").get())
+                "implementation"(libs.findLibrary("androidx.activity.ktx").get())
+                "implementation"(libs.findLibrary("androidx.fragment.ktx").get())
+                "implementation"(libs.findLibrary("androidx.recyclerview").get())
+                "implementation"(libs.findLibrary("google.material").get())
+
+                "implementation"(libs.findLibrary("kotlinx.coroutines.android").get())
+                "implementation"(libs.findLibrary("kotlinx.coroutines.core").get())
+
+                "implementation"(libs.findLibrary("timber").get())
+            }
+        }
+    }
+}
