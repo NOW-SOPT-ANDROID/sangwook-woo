f309a184300780eba57561054347c7b6e5356e9e
Sangwook123 <operawook@catholic.ac.kr>
2024-04-02 23:43:57 +0900

    [feat] #1 core module

---
+++ core/ui/build.gradle.kts
@@ -0,0 +1,26 @@
+plugins {
+    alias(libs.plugins.sopt.android.library)
+    alias(libs.plugins.sopt.android.hilt)
+}
+
+android {
+    namespace = "org.sopt.ui"
+
+    buildFeatures {
+        viewBinding = true
+    }
+}
+
+dependencies {
+    implementation(libs.androidx.appcompat)
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.activity.ktx)
+    implementation(libs.androidx.fragment.ktx)
+    implementation(libs.androidx.recyclerview)
+    implementation(libs.google.material)
+    implementation(libs.timber)
+    implementation(libs.bundles.androidx.lifecycle)
+    implementation(libs.bundles.coroutine)
+    implementation(libs.bundles.androidx.navigation)
+}
