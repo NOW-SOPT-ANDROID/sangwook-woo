5ee8ec5238238a3381334dd9885f24aef3ccf5ac
Sangwook123 <operawook@catholic.ac.kr>
2024-04-13 17:33:51 +0900

    [feat] #4 build-logic

---
+++ build-logic/convention/src/main/java/DataPlugin.kt
@@ -0,0 +1,22 @@
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.dependencies
+import org.gradle.kotlin.dsl.project
+import org.sopt.convention.libs
+
+internal class DataPlugin : Plugin<Project> {
+
+    override fun apply(target: Project) {
+        with(target) {
+            with(pluginManager) {
+                apply("sopt.android.library")
+                apply("sopt.android.hilt")
+            }
+
+            dependencies {
+                "implementation"(project(":core:model"))
+                "implementation"(libs.findBundle("coroutine").get())
+            }
+        }
+    }
+}
