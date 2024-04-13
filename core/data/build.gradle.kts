fe0f8fc64174b9e1c5afbe651da5bc19fe058b1d
Sangwook123 <operawook@catholic.ac.kr>
2024-04-13 17:39:08 +0900

    [feat] #4 data module

---
+++ core/data/build.gradle.kts
@@ -0,0 +1,15 @@
+plugins {
+    alias(libs.plugins.sopt.plugin.data)
+    alias(libs.plugins.kotlin.serialization)
+}
+
+android {
+    namespace = "org.sopt.data"
+}
+
+dependencies {
+    implementation(projects.core.domain)
+    implementation(projects.core.database)
+    implementation(projects.core.common)
+    implementation(libs.retrofit.kotlin.serialization)
+}
