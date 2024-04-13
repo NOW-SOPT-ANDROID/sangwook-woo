9f572741681d212b73be8d09732d37b964496cee
Sangwook123 <operawook@catholic.ac.kr>
2024-04-13 21:15:35 +0900

    [chore] #4 mypage module

---
+++ feature/mypage/src/main/java/org/sopt/mypage/MypageFragment.kt
@@ -0,0 +1,8 @@
+package org.sopt.mypage
+
+import org.sopt.mypage.databinding.FragmentMypageBinding
+import org.sopt.ui.base.BindingFragment
+
+class MypageFragment : BindingFragment<FragmentMypageBinding>({FragmentMypageBinding.inflate(it)}) {
+
+}
