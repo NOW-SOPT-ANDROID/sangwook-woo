e60bcabc3de6f9564e60bdb7b51685d87ecd0464
Sangwook123 <operawook@catholic.ac.kr>
2024-04-13 17:39:34 +0900

    [feat] #4 core common module

---
+++ core/common/build.gradle.kts
@@ -0,0 +1,18 @@
+@Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
+plugins {
+    alias(libs.plugins.sopt.android.library)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.sopt.android.hilt)
+}
+
+android {
+    namespace = "org.sopt.common"
+}
+
+dependencies {
+    implementation(libs.core.ktx)
+    implementation(libs.appcompat)
+    implementation(libs.material)
+    implementation(libs.bundles.coroutine)
+    implementation(libs.timber)
+}
