76f78a7f2acc1fa3d83d11bc0b9f040bb7ab7b0f
Sangwook123 <operawook@catholic.ac.kr>
2024-04-15 05:02:00 +0900

    [feat] #4 datastore module

---
+++ core/datastore/src/main/java/org/sopt/datastore/di/DataStoreModule.kt
@@ -0,0 +1,37 @@
+package org.sopt.datastore.di
+
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.core.DataStoreFactory
+import androidx.datastore.dataStore
+import androidx.datastore.dataStoreFile
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import org.sopt.common.security.SecurityUtil
+import org.sopt.datastore.UserData
+import org.sopt.datastore.UserDataSerializer
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DataStoreModule {
+    @Provides
+    @Singleton
+    fun providesUserDataStore(
+        @ApplicationContext context: Context,
+        userDataSerializer: UserDataSerializer
+    ): DataStore<UserData> =
+        DataStoreFactory.create(
+            serializer = userDataSerializer,
+        ) {
+            context.dataStoreFile("userdata.json")
+        }
+
+    /*@Provides
+    @Singleton
+    fun providesUserPreferencesDataSource(dataStore: DataStore<UserData>): UserPreferencesDataSource =
+        UserPreferencesDataSource(dataStore)*/
+}
