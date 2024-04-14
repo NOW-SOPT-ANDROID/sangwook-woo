4f314eea478708dc2f665db7b9ce09438d0cd996
Sangwook123 <operawook@catholic.ac.kr>
2024-04-15 05:04:38 +0900

    [feat] #4 mypage

--- feature/mypage/src/main/java/org/sopt/mypage/MypageFragment.kt
+++ feature/mypage/src/main/java/org/sopt/mypage/MypageFragment.kt
@@ -1,8 +1,78 @@
 package org.sopt.mypage
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import dagger.hilt.android.AndroidEntryPoint
+import org.orbitmvi.orbit.viewmodel.observe
+import org.sopt.common.intentprovider.IntentProvider
+import org.sopt.common.intentprovider.qualifier.LOGIN
 import org.sopt.mypage.databinding.FragmentMypageBinding
-import org.sopt.ui.base.BindingFragment
+import javax.inject.Inject
 
-class MypageFragment : BindingFragment<FragmentMypageBinding>({FragmentMypageBinding.inflate(it)}) {
+@AndroidEntryPoint
+class MypageFragment : Fragment() {
+    private var _binding : FragmentMypageBinding? = null
+    private val binding: FragmentMypageBinding
+        get() = requireNotNull(_binding)
+    private val viewModel by viewModels<MypageViewModel>()
 
+    @Inject
+    @LOGIN
+    lateinit var intentProvider: IntentProvider
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentMypageBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        collectState()
+        initBtnClickListener()
+    }
+
+    override fun onDestroyView() {
+        _binding = null
+        super.onDestroyView()
+    }
+
+    private fun initBtnClickListener() {
+        binding.btnMypageLogout.setOnClickListener {
+            viewModel.logout()
+        }
+
+        binding.btnMypageWithdraw.setOnClickListener {
+            viewModel.signout()
+        }
+    }
+
+    private fun collectState() {
+        viewModel.observe(viewLifecycleOwner, state = ::render, sideEffect = ::handleSideEffect)
+    }
+
+    private fun render(mypageState: MypageState) {
+        binding.tvMypageName.text = mypageState.name
+        binding.tvMypageHobby.text = mypageState.hobby
+    }
+
+    private fun handleSideEffect(sideEffect: MypageSideEffect) {
+        when(sideEffect) {
+            MypageSideEffect.LogoutSuccess -> {
+                startActivity(intentProvider.getIntent())
+                requireActivity().finish()
+            }
+            MypageSideEffect.WithdrawSuccess -> {
+                startActivity(intentProvider.getIntent())
+                requireActivity().finish()
+            }
+        }
+    }
 }
---
+++ feature/mypage/src/main/java/org/sopt/mypage/MypageViewModel.kt
@@ -0,0 +1,52 @@
+package org.sopt.mypage
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import org.orbitmvi.orbit.Container
+import org.orbitmvi.orbit.ContainerHost
+import org.orbitmvi.orbit.syntax.simple.intent
+import org.orbitmvi.orbit.syntax.simple.postSideEffect
+import org.orbitmvi.orbit.syntax.simple.reduce
+import org.orbitmvi.orbit.viewmodel.container
+import org.sopt.domain.repo.UserDataRepository
+import org.sopt.model.User
+import javax.inject.Inject
+
+@HiltViewModel
+class MypageViewModel @Inject constructor(
+    private val userDataRepository: UserDataRepository,
+) : ContainerHost<MypageState, MypageSideEffect>, ViewModel() {
+    override val container: Container<MypageState, MypageSideEffect> = container(MypageState())
+
+    init {
+        getUserData()
+    }
+
+    private fun getUserData() = intent {
+        viewModelScope.launch {
+            userDataRepository.getUserData().collect{
+                reduce { state.copy(name = it.name, hobby = it.hobby) }
+            }
+        }
+
+    }
+
+    fun logout() = intent {
+        setAutoLoginFalse()
+            .onSuccess { postSideEffect(MypageSideEffect.LogoutSuccess) }
+    }
+
+    fun signout() = intent {
+        deleteUserData()
+            .onSuccess { postSideEffect(MypageSideEffect.WithdrawSuccess) }
+    }
+
+    private suspend fun setAutoLoginFalse() =
+        runCatching { userDataRepository.setAutoLogin(false) }
+
+    private suspend fun deleteUserData() =
+        runCatching { userDataRepository.deleteUserData() }
+
+}
