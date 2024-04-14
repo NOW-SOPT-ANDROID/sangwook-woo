232ba92b108b46bb46b8083ad1822c6c9e01373a
Sangwook123 <operawook@catholic.ac.kr>
2024-04-15 05:04:13 +0900

    [feat] #4 login

--- feature/main/src/main/java/org/sopt/main/login/LoginContract.kt
+++ feature/main/src/main/java/org/sopt/main/login/LoginContract.kt
@@ -1,25 +1,26 @@
 package org.sopt.main.login
 
-import org.sopt.main.model.User
+import org.sopt.main.model.UserModel
 
 data class LoginState(
     val id: String = "",
     val password: String = "",
     val registeredId: String = "",
     val registeredPassword: String = "",
-    val name: String = "",
-    val hobby: String = ""
+    val registeredName: String = "",
+    val registeredHobby: String = "",
+    val isAutoLogin: Boolean = false,
 ){
     fun checkRegister() = registeredId.isBlank() || registeredPassword.isBlank()
 
     fun matchesUserInfo(id: String, password: String) = registeredId == id && registeredPassword == password
 
-    fun createUser() = User(registeredId, registeredPassword, name, hobby)
+    fun createUser() = UserModel(registeredId, registeredPassword, registeredName, registeredHobby)
 }
 
 sealed interface LoginSideEffect {
     data class showSnackbar(val message: String) : LoginSideEffect
-    data class LoginSuccess(val user: User) : LoginSideEffect
+    data object LoginSuccess : LoginSideEffect
     data object NavigateToSignUp : LoginSideEffect
     data object SignupSuccess : LoginSideEffect
 }
