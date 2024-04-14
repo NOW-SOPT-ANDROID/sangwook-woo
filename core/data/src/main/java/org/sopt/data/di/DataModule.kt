543c4a5e38adba8a24ae20be91c95784563d4ada
Sangwook123 <operawook@catholic.ac.kr>
2024-04-15 05:02:16 +0900

    [feat] #4 repo

--- core/data/src/main/java/org/sopt/data/di/DataModule.kt
+++ core/data/src/main/java/org/sopt/data/di/DataModule.kt
@@ -5,7 +5,9 @@ import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import org.sopt.data.repository.FriendRepository
+import org.sopt.data.repository.UserDataRepositoryImpl
 import org.sopt.domain.repo.SoptRepository
+import org.sopt.domain.repo.UserDataRepository
 import javax.inject.Singleton
 
 @Module
@@ -14,4 +16,8 @@ abstract class DataModule {
     @Singleton
     @Binds
     abstract fun bindsFriendRepo(soptRepository: FriendRepository): SoptRepository
+
+    @Singleton
+    @Binds
+    abstract fun bindsUserRepo(userDataRepository: UserDataRepositoryImpl): UserDataRepository
 }
