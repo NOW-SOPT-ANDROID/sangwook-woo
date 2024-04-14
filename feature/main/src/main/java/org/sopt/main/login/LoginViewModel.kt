8480ca9098a091c0a652688ef2fb6c04a55cc399
Sangwook123 <operawook@catholic.ac.kr>
2024-04-15 05:04:13 +0900

    [feat] #4 login

--- feature/main/src/main/java/org/sopt/main/login/LoginViewModel.kt
+++ feature/main/src/main/java/org/sopt/main/login/LoginViewModel.kt
@@ -1,7 +1,9 @@
 package org.sopt.main.login
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import org.orbitmvi.orbit.Container
 import org.orbitmvi.orbit.ContainerHost
 import org.orbitmvi.orbit.syntax.simple.intent
@@ -9,15 +11,44 @@ import org.orbitmvi.orbit.syntax.simple.postSideEffect
 import org.orbitmvi.orbit.syntax.simple.reduce
 import org.orbitmvi.orbit.viewmodel.container
 import org.sopt.designsystem.R
-import org.sopt.main.model.User
+import org.sopt.domain.repo.UserDataRepository
 import org.sopt.ui.context.ResourceProvider
 import javax.inject.Inject
 
 @HiltViewModel
 class LoginViewModel @Inject constructor(
-    private val resourceProvider: ResourceProvider
+    private val resourceProvider: ResourceProvider,
+    private val userDataRepository: UserDataRepository,
 ) : ContainerHost<LoginState, LoginSideEffect>, ViewModel() {
     override val container: Container<LoginState, LoginSideEffect> = container(LoginState())
+
+    init {
+        getUserData()
+        intent {
+            viewModelScope.launch {
+                container.stateFlow.collect{
+                    if(it.isAutoLogin){
+                        postSideEffect(LoginSideEffect.LoginSuccess)
+                    }
+                }
+            }
+        }
+    }
+    fun getUserData() = intent {
+        viewModelScope.launch {
+            userDataRepository.getUserData().collect {
+                reduce {
+                    state.copy(
+                        registeredId = it.id,
+                        registeredPassword = it.pw,
+                        registeredHobby = it.hobby,
+                        registeredName = it.name,
+                        isAutoLogin = it.autoLogin,
+                    )
+                }
+            }
+        }
+    }
 
     fun signup() = intent {
         postSideEffect(LoginSideEffect.NavigateToSignUp)
@@ -27,24 +58,14 @@ class LoginViewModel @Inject constructor(
         with(state) {
             when {
                 checkRegister() -> postSideEffect(LoginSideEffect.showSnackbar(getString(R.string.login_not_registered)))
-                matchesUserInfo(id, password) -> postSideEffect(LoginSideEffect.LoginSuccess(createUser()))
+                matchesUserInfo(id, password) -> {
+                    userDataRepository.setAutoLogin(true)
+                }
+
                 else -> postSideEffect(LoginSideEffect.showSnackbar(getString(R.string.login_login_fail)))
             }
         }
     }
 
     private fun getString(resId: Int) = resourceProvider.getString(resId)
-
-    fun signupSuccess(user: User?) = intent {
-        postSideEffect(LoginSideEffect.SignupSuccess)
-
-        reduce {
-            state.copy(
-                registeredId = user?.id.orEmpty(),
-                registeredPassword = user?.pw.orEmpty(),
-                name = user?.name.orEmpty(),
-                hobby = user?.hobby.orEmpty()
-            )
-        }
-    }
 }
