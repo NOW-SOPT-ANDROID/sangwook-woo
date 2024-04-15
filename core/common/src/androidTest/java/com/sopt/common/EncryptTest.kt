54ff0da3b37f5d8d426b80539a3ce8974806f6de
Sangwook123 <operawook@catholic.ac.kr>
2024-04-15 21:45:29 +0900

    [chore] #4 del

--- core/common/src/androidTest/java/com/sopt/common/EncryptTest.kt
+++ core/common/src/androidTest/java/com/sopt/common/EncryptTest.kt
@@ -7,11 +7,6 @@ import org.junit.Test
 import org.junit.runner.RunWith
 import org.sopt.common.security.SecurityUtil
 
-/**
- * Instrumented test, which will execute on an Android device.
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
 @RunWith(AndroidJUnit4::class)
 class EncryptTest {
     lateinit var securityUtil: SecurityUtil
