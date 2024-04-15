edcc6fc77af9614090a4d7a56da5abeab9cced43
Sangwook123 <operawook@catholic.ac.kr>
2024-04-15 20:22:40 +0900

    [feat] #4 db test

--- core/database/build.gradle.kts
+++ core/database/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     alias(libs.plugins.sopt.android.hilt)
     alias(libs.plugins.sopt.plugin.room)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.sopt.android.test)
 }
 
 android {
