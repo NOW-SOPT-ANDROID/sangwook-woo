e830920d2a423f2f867ccb64491ee576d12b0417
Sangwook123 <operawook@catholic.ac.kr>
2024-04-16 19:22:43 +0900

    [feat] #4 search navigation

---
+++ feature/search/src/main/java/org/sopt/search/SearchScreen.kt
@@ -0,0 +1,13 @@
+package org.sopt.search
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun SearchRoute(
+    label : String,
+) {
+    Box(modifier = Modifier.fillMaxSize()){}
+}
---
+++ feature/search/src/main/java/org/sopt/search/navigation/SearchNavigation.kt
@@ -0,0 +1,24 @@
+package org.sopt.search.navigation
+
+import androidx.navigation.NavController
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavOptions
+import androidx.navigation.compose.composable
+import org.sopt.search.SearchRoute
+
+fun NavController.navigateSearch(navOptions: NavOptions) {
+    navigate(SearchRoute.route, navOptions)
+}
+
+fun NavGraphBuilder.searchNavGraph(
+){
+    composable(route = SearchRoute.route) {
+        SearchRoute(
+            label = ""
+        )
+    }
+}
+
+object SearchRoute {
+    const val route = "search"
+}
