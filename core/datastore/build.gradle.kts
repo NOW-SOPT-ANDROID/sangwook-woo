e0451e81024311fae4e5f44d846667295dbbec05
Sangwook123 <operawook@catholic.ac.kr>
2024-04-16 18:55:45 +0900

    [chore] #4 datastore

---
+++ core/datastore/build.gradle.kts
@@ -0,0 +1,23 @@
+@Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
+plugins {
+    alias(libs.plugins.sopt.android.library)
+    alias(libs.plugins.sopt.android.hilt)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.serialization)
+    alias(libs.plugins.sopt.plugin.test)
+    alias(libs.plugins.sopt.android.test)
+}
+
+android {
+    defaultConfig {
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+    namespace = "org.sopt.datastore"
+}
+
+dependencies {
+    implementation(projects.core.common)
+    implementation(libs.kotlinx.serialization.json)
+    implementation(libs.bundles.coroutine)
+    implementation(libs.bundles.datastore)
+}
