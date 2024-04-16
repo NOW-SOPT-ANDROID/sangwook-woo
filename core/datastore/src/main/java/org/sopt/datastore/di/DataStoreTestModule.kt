42658f6f8a67b79c009c3ead795d8d7910b07b2e
Sangwook123 <operawook@catholic.ac.kr>
2024-04-16 18:55:45 +0900

    [chore] #4 datastore

--- core/datastore/src/main/java/org/sopt/datastore/di/DataStoreTestModule.kt
+++ core/datastore/src/main/java/org/sopt/datastore/di/DataStoreTestModule.kt
@@ -1,18 +1,14 @@
 package org.sopt.datastore.di
 
-import android.content.Context
 import androidx.datastore.core.DataStore
 import androidx.datastore.core.DataStoreFactory
-import androidx.datastore.dataStore
 import dagger.Module
 import dagger.Provides
-import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import dagger.hilt.testing.TestInstallIn
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import org.junit.rules.TemporaryFolder
-import org.sopt.common.security.SecurityUtil
 import org.sopt.datastore.UserData
 import org.sopt.datastore.UserDataSerializer
 import javax.inject.Singleton
