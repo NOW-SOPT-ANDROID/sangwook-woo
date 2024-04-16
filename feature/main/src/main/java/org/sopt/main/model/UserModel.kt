5bce0c560e6a13b3df18120e82dbd8c082385a67
Sangwook123 <operawook@catholic.ac.kr>
2024-04-16 19:55:41 +0900

    [chore] #4 usermodel

--- feature/main/src/main/java/org/sopt/main/model/UserModel.kt
+++ feature/main/src/main/java/org/sopt/main/model/UserModel.kt
@@ -2,11 +2,20 @@ package org.sopt.main.model
 
 import android.os.Parcelable
 import kotlinx.parcelize.Parcelize
+import org.sopt.model.User
 
 @Parcelize
-data class User(
+data class UserModel(
     val id: String = "",
     val pw: String = "",
     val name: String = "",
     val hobby: String = ""
 ): Parcelable
+
+fun UserModel.toUser() = User(
+    id = id,
+    pw = pw,
+    name = name,
+    hobby = hobby,
+    autoLogin = false,
+)
