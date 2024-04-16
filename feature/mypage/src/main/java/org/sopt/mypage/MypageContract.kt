77846d65fab9f2ad3f57db3807c28f997372f5ac
Sangwook123 <operawook@catholic.ac.kr>
2024-04-16 19:24:41 +0900

    [feat] #4 mypage contract

---
+++ feature/mypage/src/main/java/org/sopt/mypage/MypageContract.kt
@@ -0,0 +1,12 @@
+package org.sopt.mypage
+
+data class MypageState(
+    val name: String = "",
+    val hobby: String = "",
+)
+
+sealed interface MypageSideEffect {
+    data object LogoutSuccess : MypageSideEffect
+
+    data object WithdrawSuccess : MypageSideEffect
+}
