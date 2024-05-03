4e9768214062ccb2ca661c34f34c1d33ed0c38fb
Sangwook123 <operawook@catholic.ac.kr>
2024-05-03 12:11:15 +0900

    [feat] #7 paging source

--- core/data/build.gradle.kts
+++ core/data/build.gradle.kts
@@ -12,5 +12,8 @@ dependencies {
     implementation(projects.core.datastore)
     implementation(projects.core.database)
     implementation(projects.core.common)
+    implementation(projects.core.network)
     implementation(libs.retrofit.kotlin.serialization)
+    implementation(libs.kotlinx.serialization.json)
+    implementation(libs.androidx.paging)
 }
