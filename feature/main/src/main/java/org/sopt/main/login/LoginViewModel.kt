3de54e3e6c676dcfbe679c408abc38b70c579624
Sangwook123 <operawook@catholic.ac.kr>
2024-05-03 12:14:07 +0900

    [refactor] #7 login refactoring

--- feature/main/src/main/java/org/sopt/main/login/LoginViewModel.kt
+++ feature/main/src/main/java/org/sopt/main/login/LoginViewModel.kt
@@ -1,44 +1,33 @@
 package org.sopt.main.login
 
+import android.util.Log
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import org.orbitmvi.orbit.Container
 import org.orbitmvi.orbit.ContainerHost
 import org.orbitmvi.orbit.syntax.simple.intent
 import org.orbitmvi.orbit.syntax.simple.postSideEffect
-import org.orbitmvi.orbit.syntax.simple.reduce
 import org.orbitmvi.orbit.viewmodel.container
-import org.sopt.domain.repo.UserDataRepository
+import org.sopt.domain.repo.AuthRepository
+import org.sopt.main.login.navigation.LoginRoute
 import org.sopt.ui.orbit.updateState
 import javax.inject.Inject
 
 @HiltViewModel
 class LoginViewModel @Inject constructor(
-    private val userDataRepository: UserDataRepository,
+    private val authRepository: AuthRepository,
+    savedStateHandle: SavedStateHandle,
 ) : ContainerHost<LoginState, LoginSideEffect>, ViewModel() {
     override val container: Container<LoginState, LoginSideEffect> = container(LoginState())
 
-    init {
-        getUserData()
-        intent {
-            container.stateFlow.collect {
-                if (it.isAutoLogin) {
-                    postSideEffect(LoginSideEffect.LoginSuccess)
-                }
-            }
-        }
-    }
+    private val isSignupSuccess: Boolean =
+        savedStateHandle.get<String>(LoginRoute.IS_SIGNUP_SUCESS).toBoolean()
 
-    fun getUserData() = intent {
-        userDataRepository.getUserData().collect {
-            reduce {
-                state.copy(
-                    registeredId = it.id,
-                    registeredPassword = it.pw,
-                    registeredHobby = it.hobby,
-                    registeredName = it.name,
-                    isAutoLogin = it.autoLogin,
-                )
+    init {
+        intent {
+            if (isSignupSuccess) {
+                postSideEffect(LoginSideEffect.SignupSuccess)
             }
         }
     }
@@ -48,19 +37,20 @@ class LoginViewModel @Inject constructor(
     }
 
     fun login() = intent {
-        with(state) {
-            when {
-                checkRegister() -> {
-                    postSideEffect(LoginSideEffect.showSnackbar("회원가입 먼저하셈"))
-                }
-                matchesUserInfo(state.id, state.password) -> {
-                    userDataRepository.setAutoLogin(true)
-                }
-
-                else -> postSideEffect(LoginSideEffect.showSnackbar("로그인 실패"))
+        authRepository.postSignin(
+            id = state.id,
+            pw = state.password
+        ).onSuccess {
+            if (it.code !in 200..299) {
+                postSideEffect(LoginSideEffect.showSnackbar(it.message))
+            } else {
+                postSideEffect(LoginSideEffect.LoginSuccess)
             }
+        }.onFailure {
+            Log.e("throwable", it.toString())
         }
     }
+
 
     fun updateId(id: String) = updateState { copy(id = id) }
     fun updatePw(pw: String) = updateState { copy(password = pw) }
